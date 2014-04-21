9eaee6039807d9c597c005cadd198f32c7fc804e
jiezhang <jiezhang@marvell.com>
2014-04-21 11:38:11 +0800

    Update GC driver to version rls-pxa1928-kk44-alpha1-r3
    1) Fix a race condition in kernel event management
    
    Change-Id: I92dc887195d86a52a0de0729a48ec804374db415
    Signed-off-by: jiezhang <jiezhang@marvell.com>

--- hal/kernel/gc_hal_kernel_event.c
+++ hal/kernel/gc_hal_kernel_event.c
@@ -194,10 +194,8 @@ _TryToIdleGPU(
 {
     gceSTATUS status;
     gctBOOL empty = gcvFALSE, idle = gcvFALSE;
-    gctUINT32 process, thread;
     gctBOOL powerLocked = gcvFALSE;
     gckHARDWARE hardware;
-
 
     gcmkHEADER_ARG("Event=0x%x", Event);
 
@@ -208,7 +206,6 @@ _TryToIdleGPU(
     hardware = Event->kernel->hardware;
     gcmkVERIFY_OBJECT(hardware, gcvOBJ_HARDWARE);
 
-
     /* Check whether the event queue is empty. */
     gcmkONERROR(gckEVENT_IsEmpty(Event, &empty));
 
@@ -217,30 +214,17 @@ _TryToIdleGPU(
         status = gckOS_AcquireRecMutex(hardware->os, hardware->recMutexPower, 0);
         if (status == gcvSTATUS_TIMEOUT)
         {
-            gcmkONERROR(gckOS_GetProcessID(&process));
-            gcmkONERROR(gckOS_GetThreadID(&thread));
+            gcmkFOOTER_NO();
+            return gcvSTATUS_OK;
+        }
 
-            /* Just return to prevent deadlock. */
-            if ((hardware->powerProcess != process)
-            ||  (hardware->powerThread  != thread))
-            {
-                gcmkFOOTER_NO();
-                return gcvSTATUS_OK;
-            }
-        }
-        else
-        {
-            powerLocked = gcvTRUE;
-        }
+        powerLocked = gcvTRUE;
 
         /* Query whether the hardware is idle. */
         gcmkONERROR(gckHARDWARE_QueryIdle(Event->kernel->hardware, &idle));
 
-        if (powerLocked)
-        {
-            gcmkONERROR(gckOS_ReleaseRecMutex(hardware->os, hardware->recMutexPower));
-            powerLocked = gcvFALSE;
-        }
+        gcmkONERROR(gckOS_ReleaseRecMutex(hardware->os, hardware->recMutexPower));
+        powerLocked = gcvFALSE;
 
         if (idle)
         {
@@ -255,7 +239,6 @@ _TryToIdleGPU(
     return gcvSTATUS_OK;
 
 OnError:
-
     if (powerLocked)
     {
         gcmkVERIFY_OK(gckOS_ReleaseRecMutex(hardware->os, hardware->recMutexPower));
