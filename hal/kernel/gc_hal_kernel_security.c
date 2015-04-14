287d7899051f92d3b0c709a110a7fb644e61e6ed
jiezhang <jiezhang@marvell.com>
2015-04-14 15:25:59 +0800

    add GPL header
    Change-Id: I96eebe778ba6cd690761a1e60bbf8335eb08342b
    Signed-off-by: jiezhang <jiezhang@marvell.com>
    (cherry picked from commit 7bc757999f6d112850901105dd4a21c4a19afe58)

--- hal/kernel/gc_hal_kernel_security.c
+++ hal/kernel/gc_hal_kernel_security.c
@@ -1,12 +1,21 @@
 /****************************************************************************
 *
-*    Copyright (c) 2005 - 2015 by Vivante Corp.  All rights reserved.
+*    Copyright (c) 2005 - 2015 by Vivante Corp.
+*    
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the license, or
+*    (at your option) any later version.
+*    
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*    GNU General Public License for more details.
+*    
+*    You should have received a copy of the GNU General Public License
+*    along with this program; if not write to the Free Software
+*    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 *
-*    The material in this file is confidential and contains trade secrets
-*    of Vivante Corporation. This is proprietary information owned by
-*    Vivante Corporation. No part of this work may be disclosed,
-*    reproduced, copied, transmitted, or used in any way for any purpose,
-*    without the express written permission of Vivante Corporation.
 *
 *****************************************************************************/
 
